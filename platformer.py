34c76caaa3caa146dc40d1577bd0161de23cf59a
dizizmi <65617120+dizizmi@users.noreply.github.com>
2023-04-08 15:07:45 +0800

    Brick bg onto grid

--- platformer.py
+++ platformer.py
@@ -9,18 +9,69 @@ screen_height = 1000
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 
+#define game variables 
+tile_size = 200
+
+
+
 #load image
 sun_img =  pygame.image.load('assets/sun_img.png')
 bg_img = pygame.image.load('assets/bg.png')
 
 def draw_grid():
     for line in range(0,6):
-        
+        pygame.draw.line(screen, (255,255,255), (0, line * tile_size), (screen_width, line * tile_size))
+        pygame.draw.line(screen, (255,255,255), (line * tile_size,0), (line * tile_size, screen_height))
+
+class World():
+    def __init__(self,data):
+        #adding as tuple
+        self.tile_list = []
+        #load images
+        dirt_img = pygame.image.load('assets/brick.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    #adding as tuple, adding tiles and coordinates into 1
+                    self.tile_list.append(tile)
+
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+
+#for every grid
+world_data = [
+[1,1,1,1,1],
+[1,0,0,0,1],
+[1,0,0,0,1],
+[1,0,0,0,1],
+[1,1,1,1,1],
+]
+
+world = World(world_data)
+
 run = True
 while run:
 
     screen.blit(bg_img, (0,0))
     screen.blit(sun_img, (100,100))
+
+    world.draw()
+    draw_grid()
+
+    print(world.tile_list)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
